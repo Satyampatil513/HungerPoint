c4d6bcd6e6b4e12a2f0a41ca4921f6a7aa3dc45f
Satyampatil513 <b21223@students.iitmandi.ac.in>
2024-12-15 17:06:02 +0530

    added port

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+// vite.config.js
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+  server: {
+    port: process.env.PORT || 3000,
+    host: true, // This will expose the server to the network
+  },
+  preview: {
+    port: process.env.PORT || 3000,
+    host: true, // This will expose the server to the network
+  },
+});
